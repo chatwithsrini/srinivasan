26ddb21d2c160e2b46b8e12b3957ad66feaac31f
Srinivasan Sekar <srinivasans@presidio.com>
2025-09-06 14:44:54 +0530

    Fix development server to work without /srinivasan subdirectory

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/srinivasan/",
+  base: command === 'build' ? "/srinivasan/" : "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
   },
-});
+}));
