a29f9fa40f648e5600a08e38f142c2f060a86300
Srinivasan Sekar <srinivasans@presidio.com>
2025-09-06 15:19:04 +0530

    updated with vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? "/srinivasan/" : "/",
+  base: command === "build" ? "/srinivasan/" : "/",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
+    assetsDir: "src/assets",
+    rollupOptions: {
+      output: {
+        // Keep original names for all asset files
+        assetFileNames: "src/assets/[name][extname]",
+        chunkFileNames: "src/assets/[name].js",
+        entryFileNames: "src/assets/[name].js",
+      },
+    },
   },
 }));
