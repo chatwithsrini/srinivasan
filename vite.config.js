31baea984f34ed97088ab92a20369536397f80cf
Srinivasan Sekar <srinivasans@presidio.com>
2025-09-07 16:05:49 +0530

    build changes complete

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/srinivasan/" : "/",
+  base: "/",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
+    assetsDir: "src/assets",
     emptyOutDir: true,
     rollupOptions: {
       output: {
